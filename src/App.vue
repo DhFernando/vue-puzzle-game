bb626e513eaca128494e461f7a9b0113b3f560f7
dhfernando <hdilshan.fernando11@gmail.com>
2020-03-26 01:59:21 +0530

    init puzzel game repo

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template  >
+  <div id="app"  >
+      <ticTacToe />
+  </div>
+</template>
+
+<script>
+import ticTacToe from './components/puzzle'
+
+export default {
+  name: 'app',
+  components: {
+    ticTacToe
+  },
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+ 
+}
+</style>
---
+++ src/components/puzzle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'puzzle',
+  
+  data(){
+      return{
+        
+      }
+    },
+    computed: {
+      
+    },
+    methods:{
+      
+    }
+}
+</script>
+
+
+<style scoped>
+ 
+</style>
